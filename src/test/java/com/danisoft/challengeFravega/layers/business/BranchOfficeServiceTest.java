d5c3966733d46507687b3fded69f67234d98aaae
DanielChungara <danielchungara1@gmail.com>
2021-06-23 15:11:46 -0300

    Create Branch Office Failure test. #cf14

--- src/test/java/com/danisoft/challengeFravega/layers/business/BranchOfficeServiceTest.java
+++ src/test/java/com/danisoft/challengeFravega/layers/business/BranchOfficeServiceTest.java
@@ -17,14 +17,14 @@ class BranchOfficeServiceTest extends TestContainersBase {
     @Autowired
     BranchOfficeService service;
 
+
+    final String ADDRESS = "Av. Corrientes 3889, Capital Federal";
+    final String ATTENTION = "LU/SA: 10:00 a 18:00hs";
+    final BigDecimal LATITUDE = BigDecimal.valueOf(-34.603002);
+    final BigDecimal LONGITUDE = BigDecimal.valueOf(-58.419087);
+
     @Test
     void createBranchOfficeByDtoSuccess() {
-
-        final String ADDRESS = "Av. Corrientes 3889, Capital Federal";
-        final String ATTENTION = "LU/SA: 10:00 a 18:00hs";
-        final BigDecimal LATITUDE = BigDecimal.valueOf(-34.603002);
-        final BigDecimal LONGITUDE = BigDecimal.valueOf(-58.419087);
-
 
         BranchOfficeDto dto = BranchOfficeDto.builder()
                 .address(ADDRESS)
@@ -43,5 +43,17 @@ class BranchOfficeServiceTest extends TestContainersBase {
         assertEquals(LONGITUDE, model.getLongitude());
 
         log.info(">>> Branch Office created with ID {}", model.getId().toString());
+    }
+
+    @Test
+    void createBranchOfficeByDtoWithoutCoordinates() {
+
+        BranchOfficeDto dto = BranchOfficeDto.builder()
+                .address(ADDRESS)
+                .attention(ATTENTION)
+                .build();
+
+        assertThrows(BusinessException.class, () -> this.service.createByDto(dto));
+
     }
 }
